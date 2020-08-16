2df446dbb67257dd160c25beaf144726f2460832
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-08-16 19:42:27 +0700

    Init

---
+++ database/migrations/2020_08_16_123203_create_podcasts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePodcastsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('podcasts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 150);
+            $table->text('description');
+            $table->string('file_mp3', 150)->nullable();
+            $table->string('file_ogg', 150)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('podcasts');
+    }
+}
